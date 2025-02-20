28ec1c5890e96e58c8fbeeb6003873ff330d66fe
Matt Radford <quinquex@gmail.com>
2025-02-20 14:12:33 -0800

    show apps

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/user');
+const User = require('../models/user.js');
 
 // GET /users/:userId/applications
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications,
+        });
     } catch (err) {
         console.log(err);
         res.redirect('/');
@@ -16,6 +19,19 @@ router.get('/', (req, res) => {
 // GET /users/:userId/applications/new
 router.get('/new', (req, res) => {
     res.render('applications/new.ejs');
+});
+
+// POST /users/:userId/applications
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
 });
 
 module.exports = router;
