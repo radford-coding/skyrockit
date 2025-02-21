419ce375dbd10e217243ddf6304472ecaf921b9e
Matt Radford <quinquex@gmail.com>
2025-02-21 09:53:41 -0800

    finished CRUD

--- controllers/applications.js
+++ controllers/applications.js
@@ -34,4 +34,58 @@ router.post('/', async (req, res) => {
     };
 });
 
+// GET /users/:userId/applications/:applicationId
+router.get('/:applicationId', async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id);
+        const application = user.applications.id(req.params.applicationId);
+        res.render('applications/show.ejs', { application, user, });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
+
+// DELETE /users/:userId/applications/:applicationId
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.id(req.params.applicationId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
+
+// GET /users/:userId/applications/:applicationId/edit
+router.get('/:applicationId/edit', async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id);
+        const application = user.applications.id(req.params.applicationId);
+        res.render('applications/edit.ejs', { application, user, });
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
+
+// PUT /users/:userId/applications/:applicationId
+router.put('/:applicationId', async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id);
+        console.log(req.body);
+        const application = user.applications.id(req.params.applicationId);
+        application.set(req.body);
+        await user.save();
+        res.redirect(`/users/${user._id}/applications/${req.params.applicationId}`);
+    } catch (err) {
+        console.log(err);
+        res.redirect('/');
+    };
+});
+
+
+
 module.exports = router;
